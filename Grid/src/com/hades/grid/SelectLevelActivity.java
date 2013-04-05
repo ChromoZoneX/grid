02268949c6164470a02c4b0180cbe8fec8a8c65c
huzefa <huzefa@huz-gt60>
2013-04-05 01:36:40 -0400

    - Added empty classes for levels - Fixed listView to launch levels

--- Grid/src/com/hades/grid/SelectLevelActivity.java
+++ Grid/src/com/hades/grid/SelectLevelActivity.java
@@ -3,9 +3,12 @@ package com.hades.grid;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -26,7 +29,7 @@ public class SelectLevelActivity extends Activity{
 			}
 		});
 		
-		ListView lv = (ListView) findViewById(R.id.levelList);
+		final ListView lv = (ListView) findViewById(R.id.levelList);
 		ArrayList<String> levelList = new ArrayList<String>(); 
 		levelList.add("One   (5 X 5)");
 		levelList.add("Two   (6 X 6)");
@@ -34,6 +37,46 @@ public class SelectLevelActivity extends Activity{
 		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, 
 				android.R.layout.simple_list_item_1, levelList);
 		lv.setAdapter(arrayAdapter);
+		
+		lv.setClickable(true);
+		lv.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View view, int position,
+					long id) {
+				if (position == 0){
+					Intent i = new Intent(SelectLevelActivity.this, Level1.class);
+					startActivityForResult(i, 0);
+				}
+				else if (position == 1){
+					Intent i = new Intent(SelectLevelActivity.this, Level2.class);
+					startActivityForResult(i,1);
+				}
+				else if (position == 2){
+					Intent i = new Intent(SelectLevelActivity.this, Level3.class);
+					startActivityForResult(i,2);
+				}
+				
+			}
+		});
+		
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		if (requestCode == 0 && resultCode == RESULT_OK){
+			/* Save level achievement */
+			finishActivity();
+		}
+		else if (requestCode == 1 && resultCode == RESULT_OK){
+			/* Save level achievement */
+			finishActivity();
+
+		}
+		else if (requestCode == 2 && resultCode == RESULT_OK){
+			/* Save level achievement */
+			finishActivity();
+		}
 	}
 	
 	private void finishActivity(){
