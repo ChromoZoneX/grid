30902f640f377b0aea9f479a8e93581e2fe3234e
huzefa <huzefa@huz-gt60>
2013-04-05 13:11:37 -0400

    Added new 5X5 grid. Non functional as of now. Also added a helper class.

--- Grid/src/com/hades/grid/Level1.java
+++ Grid/src/com/hades/grid/Level1.java
@@ -11,8 +11,7 @@ public class Level1 extends Activity {
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_level_1);
-		
-		ImageButton[][] iButton = new ImageButton[5][5];
+
 		
 	}
 	
