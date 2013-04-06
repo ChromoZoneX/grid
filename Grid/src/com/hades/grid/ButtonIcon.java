8162a8e384919b6dbda54486b3c0ad63686f0382
huzefa <huzefa@huz-gt60>
2013-04-05 21:14:28 -0400

    Changed all sprites for resizing. Switched variable names to make them more convenient. Loaded all images on to 5x5 grid and made them fit.

--- Grid/src/com/hades/grid/ButtonIcon.java
+++ Grid/src/com/hades/grid/ButtonIcon.java
@@ -2,34 +2,47 @@ package com.hades.grid;
 
 public class ButtonIcon{
 	
-	//Load sprite
-	public static int load0 = R.drawable.load0;
-	public static int load1 = R.drawable.load1;
-	public static int load2 = R.drawable.load2;
-	public static int load3 = R.drawable.load3;
+	public static int[] l = {
+		//Load sprite
+		R.drawable.load0,
+		R.drawable.load1,
+		R.drawable.load2,
+		R.drawable.load3
+	};
 	
-	//Power sprite
-	public static int power0 = R.drawable.power0;
-	public static int power1 = R.drawable.power1;
-	public static int power2 = R.drawable.power2;
-	public static int power3 = R.drawable.power3;
-	public static int power4 = R.drawable.power4;
-	public static int power5 = R.drawable.power5;
+	public static int[] p = {
+		//Power sprite
+		R.drawable.power0,
+		R.drawable.power1,
+		R.drawable.power2,
+		R.drawable.power3,
+		R.drawable.power4,
+		R.drawable.power5
+	};
 	
-	//Right angled wire sprite
-	public static int right0 = R.drawable.right0;
-	public static int right1 = R.drawable.right1;
-	public static int right2 = R.drawable.right2;
-	public static int right3 = R.drawable.right3;
-
+	public static int[] r = {
+		//Right angled wire sprite
+		R.drawable.right0,
+		R.drawable.right1,
+		R.drawable.right2,
+		R.drawable.right3
+	};
 	
-	//Straight wire sprite
-	public static int straight0 = R.drawable.straight0;
-	public static int straight1 = R.drawable.straight1;
-
+	public static int[] s = {
+		//Straight wire sprite
+		R.drawable.straight0,
+		R.drawable.straight1
+	};
+	
+	public static int[] t = {
 	//T-shaped wire sprite
-	public static int t0 = R.drawable.t0;
-	public static int t1 = R.drawable.t1;
-	public static int t2 = R.drawable.t2;
-	public static int t3 = R.drawable.t3;
+	R.drawable.t0,
+	R.drawable.t1,
+	R.drawable.t2,
+	R.drawable.t3
+	};
+	
+	public static void changeIcon(int[] id){
+		
+	}
 }
--- Grid/src/com/hades/grid/Level1.java
+++ Grid/src/com/hades/grid/Level1.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class Level1 extends Activity {
@@ -16,50 +17,82 @@ public class Level1 extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_level_1);
 		
-		ImageButton[][] gridButton = new ImageButton[5][5];
+		final ImageButton[][] gridButton = new ImageButton[5][5];
 		
+		/*****************Populating the  grid*********************************/
 		//First row
 		gridButton[0][0] = (ImageButton) findViewById(R.id.imageButton1);
 		gridButton[0][1] = (ImageButton) findViewById(R.id.ImageButton01);
 		gridButton[0][2] = (ImageButton) findViewById(R.id.ImageButton24);
 		gridButton[0][3] = (ImageButton) findViewById(R.id.ImageButton23);
 		gridButton[0][4] = (ImageButton) findViewById(R.id.ImageButton22);
+		
+		gridButton[0][0].setImageResource(ButtonIcon.l[0]);
+		gridButton[0][1].setImageResource(ButtonIcon.r[3]);
+		gridButton[0][2].setImageResource(ButtonIcon.s[1]);
+		gridButton[0][3].setImageResource(ButtonIcon.l[0]);
+		gridButton[0][4].setImageResource(ButtonIcon.l[0]);
+		
 		//Second row
 		gridButton[1][0] = (ImageButton) findViewById(R.id.ImageButton21);
 		gridButton[1][1] = (ImageButton) findViewById(R.id.ImageButton17);
 		gridButton[1][2] = (ImageButton) findViewById(R.id.ImageButton13);
 		gridButton[1][3] = (ImageButton) findViewById(R.id.ImageButton11);
 		gridButton[1][4] = (ImageButton) findViewById(R.id.ImageButton10);
+		
+		gridButton[1][0].setImageResource(ButtonIcon.s[1]);
+		gridButton[1][1].setImageResource(ButtonIcon.s[0]);
+		gridButton[1][2].setImageResource(ButtonIcon.r[3]);
+		gridButton[1][3].setImageResource(ButtonIcon.l[1]);
+		gridButton[1][4].setImageResource(ButtonIcon.s[1]);
+		
 		//Third row
 		gridButton[2][0] = (ImageButton) findViewById(R.id.ImageButton20);
 		gridButton[2][1] = (ImageButton) findViewById(R.id.ImageButton16);
 		gridButton[2][2] = (ImageButton) findViewById(R.id.ImageButton12);
 		gridButton[2][3] = (ImageButton) findViewById(R.id.ImageButton09);
 		gridButton[2][4] = (ImageButton) findViewById(R.id.ImageButton08);
+		
+		gridButton[2][0].setImageResource(ButtonIcon.t[2]);
+		gridButton[2][1].setImageResource(ButtonIcon.r[0]);
+		gridButton[2][2].setImageResource(ButtonIcon.s[0]);
+		gridButton[2][3].setImageResource(ButtonIcon.r[2]);
+		gridButton[2][4].setImageResource(ButtonIcon.t[1]);
+		
 		//Fourth row
-		gridButton[1][0] = (ImageButton) findViewById(R.id.ImageButton19);
-		gridButton[1][1] = (ImageButton) findViewById(R.id.ImageButton15);
-		gridButton[1][2] = (ImageButton) findViewById(R.id.ImageButton07);
-		gridButton[1][3] = (ImageButton) findViewById(R.id.ImageButton05);
-		gridButton[1][4] = (ImageButton) findViewById(R.id.ImageButton03);
+		gridButton[3][0] = (ImageButton) findViewById(R.id.ImageButton19);
+		gridButton[3][1] = (ImageButton) findViewById(R.id.ImageButton15);
+		gridButton[3][2] = (ImageButton) findViewById(R.id.ImageButton07);
+		gridButton[3][3] = (ImageButton) findViewById(R.id.ImageButton05);
+		gridButton[3][4] = (ImageButton) findViewById(R.id.ImageButton03);
+		
+		gridButton[3][0].setImageResource(ButtonIcon.t[3]);
+		gridButton[3][1].setImageResource(ButtonIcon.l[1]);
+		gridButton[3][2].setImageResource(ButtonIcon.s[1]);
+		gridButton[3][3].setImageResource(ButtonIcon.s[0]);
+		gridButton[3][4].setImageResource(ButtonIcon.s[1]);
+		
 		//Fifth row
-		gridButton[1][0] = (ImageButton) findViewById(R.id.ImageButton18);
-		gridButton[1][1] = (ImageButton) findViewById(R.id.ImageButton14);
-		gridButton[1][2] = (ImageButton) findViewById(R.id.ImageButton06);
-		gridButton[1][3] = (ImageButton) findViewById(R.id.ImageButton04);
-		gridButton[1][4] = (ImageButton) findViewById(R.id.ImageButton02);
+		gridButton[4][0] = (ImageButton) findViewById(R.id.ImageButton18);
+		gridButton[4][1] = (ImageButton) findViewById(R.id.ImageButton14);
+		gridButton[4][2] = (ImageButton) findViewById(R.id.ImageButton06);
+		gridButton[4][3] = (ImageButton) findViewById(R.id.ImageButton04);
+		gridButton[4][4] = (ImageButton) findViewById(R.id.ImageButton02);
 		
-		gridButton[0][0].setImageResource(ButtonIcon.power0);
-		gridButton[0][1].setImageResource(ButtonIcon.power0);
-		gridButton[0][2].setImageResource(ButtonIcon.power0);
-		gridButton[0][3].setImageResource(ButtonIcon.power0);
-		gridButton[0][4].setImageResource(ButtonIcon.power0);
+		gridButton[4][0].setImageResource(ButtonIcon.r[2]);
+		gridButton[4][1].setImageResource(ButtonIcon.p[5]);
+		gridButton[4][2].setImageResource(ButtonIcon.t[2]);
+		gridButton[4][3].setImageResource(ButtonIcon.r[0]);
+		gridButton[4][4].setImageResource(ButtonIcon.l[1]);
+		/**********************************************************************/
 		
-//		for (int i = 0; i < 5; i++){
-//			for (int j = 0; j < 5; j++){
-//				gridButton[i][j].invalidate();
-//			}
-//		}
+		gridButton[0][0].setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				ButtonIcon.changeIcon(gridButton[0][0].getDrawableState());
+			}
+		});
 		
 	}
 	
